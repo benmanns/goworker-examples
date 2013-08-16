9c66015b2d3809c6ac8ccbed91620e64dc3d14b9
Benjamin Manns <benmanns@gmail.com>
2013-08-15 23:49:19 -0400

    Create Hello worker.

---
+++ hello_worker.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/benmanns/goworker"
+)
+
+func init() {
+	goworker.Register("Hello", helloWorker)
+}
+
+func helloWorker(queue string, args ...interface{}) error {
+	fmt.Println("Hello, world!")
+	return nil
+}
